f5b01218b5bbdf460d295113a21d5321552c83d1
tgmattso <tim@timmattson.com>
2024-10-17 17:01:31 +0200

    added floating Point Arith Exercises

---
+++ hands-on/flop/UtilityFunctions.h
@@ -0,0 +1,7 @@
+//  The utility functions used in the summation program follow: 
+
+void seed(float, float);                       // seed pseudorandom number generator
+float frandom();                                 // Get next pseudorandom in the sequence
+void FillSequence(float*, float*);       // fill sequence (>0), return "true" sum
+int comp(const void *, const void *); // comparator func for qsort
+
