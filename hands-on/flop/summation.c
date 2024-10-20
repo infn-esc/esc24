c435db652306ef0e5b9e9315511e22b3be506d00
tgmattso <tim@timmattson.com>
2024-10-20 19:17:17 +0200

    final FLOP exercises

--- hands-on/flop/summation.c
+++ hands-on/flop/summation.c
@@ -12,13 +12,15 @@ Usage:
    UtilityFunctions.c.   For reasons of pedagogy, do not look
    at the functions in UtilityFunctions.c. Treat it as a block box.
 
+        gcc summation.c UtilityFunctions.c
+
 History: 
    Written by Tim Mattson, 9/2023.
 
 */
 #include <stdio.h>
 #include <stdlib.h>
-#include "UtilityFunctions.h"
+#include "UtilityFunctions.h"  // where FillSequence comes from
 
 #define N 100000  //length of sequence of numbers to work with  
 
@@ -29,38 +31,10 @@ int main ()
    float seq[N];    //Sequence to sum, statically defined to keep the code simple
    float True_sum;  //The best estimate of the actual sum
 
-   FillSequence(seq, &True_sum); // populate seq with random values > 0
+   FillSequence(N, seq, &True_sum); // populate seq with N random values > 0
 
-   // basic, naive sum
-   float sum_val =0.0f;
-   for (int i=0; i<N; i++){
-       sum_val += seq[i];
-   }
-   printf(" Basic sum = %f, best estimated sum = %f\n",sum_val,True_sum);
+// Add code here to sum the elements of the array of length N, seq
+
+   printf(" Estimated sum = %f\n",True_sum);
 
 }
-
-/**********************************************************************************************
-//  This is a partial implementatioin of Kahan's compensated summation.  Using
-//   what we've discussed about errors in addition and subtraction, complete
-//   figure out how to compute the error correction term
-
-float KahanSum(int Nlen, float *x)
-{
-   float ksum, correction, xcorrected, tmpSum;
-
-   if(Nlen < 1) return -1;  // need at least two values to sum
-
-   correction = 0.0f;
-   ksum = 0.0f;
-   for (int i = 0; i<Nlen;i++){
-      xcorrected = x[i] ?? correction; 
-      tmpSum =  ksum + xcorrected;
-      
-      correction = ??????????;
-      
-      ksum = tmpSum;
-   }
-   return ksum;
-}
-**********************************************************************************************/
